90465343a036b59d1321eaf8a54e450413fa41f6
HongHua <331187632@qq.com>
2024-06-14 20:19:39 +0800

    内容修改

--- util/misc.py
+++ util/misc.py
@@ -18,7 +18,16 @@ from pathlib import Path
 
 import torch
 import torch.distributed as dist
-from torch._six import inf
+import torch
+
+TORCH_MAJOR = int(torch.__version__.split('.')[0])
+TORCH_MINOR = int(torch.__version__.split('.')[1])
+
+if TORCH_MAJOR == 1 and TORCH_MINOR < 8:
+    from torch._six import inf
+else:
+    from math import inf
+
 
 
 class SmoothedValue(object):
